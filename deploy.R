57214b5fe3e66d64ad7ee356f6986f9ea5e72322
Hui Ying <wonghuiying.why@gmail.com>
2025-04-03 15:57:53 +0800

    Fix deploy.r installation permission error

--- deploy.R
+++ deploy.R
@@ -1,4 +1,18 @@
 
-# add to install any new package required
+# Create a project-specific library directory
+lib_dir <- "/srv/shiny-server/Bioinformatics-Analysis-of-Proteomic-Data-MCS08/r_libs"
+dir.create(lib_dir, recursive = TRUE, showWarnings = FALSE)
 
-install.packages(c("shiny", "ggplot2", "dplyr", "testthat"), repos="http://cran.rstudio.com")
+# Define required packages
+required_packages <- c("shiny", "ggplot2", "dplyr", "testthat", "shinythemes",
+                       "limma", "pheatmap", "RColorBrewer")
+
+
+# Install missing packages to the project library
+new_packages <- required_packages[!required_packages %in% installed.packages(lib.loc = lib_dir)[,"Package"]]
+if(length(new_packages) > 0) {
+  install.packages(new_packages, lib = lib_dir, repos = "https://cran.rstudio.com/")
+}
+
+# Add this library to the search path
+.libPaths(c(lib_dir, .libPaths()))
