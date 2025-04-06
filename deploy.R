6fbe6fd15074bb37827273222cca050b9fb882f5
Hui Ying <wonghuiying.why@gmail.com>
2025-04-06 14:57:30 +0800

    add new dependencies and try catch to view error and prevent failure

--- deploy.R
+++ deploy.R
@@ -1,19 +1,32 @@
 
 # Create a project-specific library directory
 lib_dir <- "/srv/shiny-server/Bioinformatics-Analysis-of-Proteomic-Data-MCS08/r_libs"
-dir.create(lib_dir, recursive = TRUE, showWarnings = FALSE)
+if (!dir.exists(lib_dir)) {
+  dir.create(lib_dir, recursive = TRUE, showWarnings = FALSE)
+}
 
 # Define required packages
+if (!"BiocManager" %in% installed.packages()[,"Package"]) {
+  install.packages("BiocManager", repos = "https://cran.rstudio.com/")
+}
 required_packages <- c("shiny", "ggplot2", "dplyr", "testthat", "shinythemes",
-                       "limma", "pheatmap", "RColorBrewer","httr","jsonlite",
-                       "pool", "")
+                       "pheatmap", "RColorBrewer","httr","jsonlite",
+                       "pool", "DBI")
+bioc_packages <- c("limma")
 
 
 # Install missing packages to the project library
-new_packages <- required_packages[!required_packages %in% installed.packages(lib.loc = lib_dir)[,"Package"]]
-if(length(new_packages) > 0) {
-  install.packages(new_packages, lib = lib_dir, repos = "https://cran.rstudio.com/")
+for (pkg in required_packages) {
+  if (!pkg %in% installed.packages(lib.loc = lib_dir)[,"Package"]) {
+    tryCatch({
+      install.packages(pkg, lib = lib_dir, repos = "https://cran.rstudio.com/", dependencies = TRUE)
+      cat(paste0("Successfully installed ", pkg, "\n"))
+    }, error = function(e) {
+      cat(paste0("Failed to install ", pkg, ": ", e$message, "\n"))
+    })
+  }
 }
+
 
 # Add this library to the search path
 .libPaths(c(lib_dir, .libPaths()))
