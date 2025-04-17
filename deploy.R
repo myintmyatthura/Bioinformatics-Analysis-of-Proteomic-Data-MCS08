886c731db1e5b4d8f6aba53cee7b6572ee8c6e4c
myintmyatthura <myintmyatthura.jr@gmail.com>
2025-04-17 16:39:25 +0800

    new dependencies

--- deploy.R
+++ deploy.R
@@ -11,7 +11,7 @@ if (!"BiocManager" %in% installed.packages()[,"Package"]) {
 }
 required_packages <- c("shiny", "ggplot2", "dplyr", "testthat", "shinythemes",
                        "pheatmap", "RColorBrewer", "httr", "jsonlite",
-                       "pool", "DBI", "tinytex", "rmarkdown", "knitr","kableExtra","readxl","magick")
+                       "pool", "DBI", "tinytex", "rmarkdown", "knitr","kableExtra","readxl","magick","ggrepel")
 bioc_packages <- c("limma")
 
 
